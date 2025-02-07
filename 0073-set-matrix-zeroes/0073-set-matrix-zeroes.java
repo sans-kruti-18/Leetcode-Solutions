a61209e7371be2dbf3ef740c20b93144dea7f04d
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2025-02-07 16:09:04 +0530

    Time: 1 ms (74.5%), Space: 45.6 MB (65.57%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int col0 = 1;
+
+        // Step 1: Traverse the matrix and mark the first row & column accordingly
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // Mark i-th row
+                    matrix[i][0] = 0;
+                    // Mark j-th column
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        // Step 2: Mark cells with 0 from (1,1) to (n-1, m-1)
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // Step 3: Mark the first row and first column
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+    }
+}
