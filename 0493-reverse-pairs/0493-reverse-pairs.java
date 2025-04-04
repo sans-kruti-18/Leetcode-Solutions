be1c68e2bc01cd1682d84080af71eb217d8c293f
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2025-04-04 20:43:28 +0530

    Time: 78 ms (37.61%), Space: 54.1 MB (89.58%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,32 +1,31 @@
-import java.util.ArrayList;
-
 class Solution {
     public int reversePairs(int[] nums) {
         return mergeSort(nums, 0, nums.length - 1);
     }
 
     private int mergeSort(int[] arr, int low, int high) {
-        if (low >= high) return 0;
+        int cnt = 0;
+        if (low >= high) return cnt;
         int mid = (low + high) / 2;
-        int count = mergeSort(arr, low, mid);  
-        count += mergeSort(arr, mid + 1, high);  
-        count += countPairs(arr, low, mid, high);  
-        merge(arr, low, mid, high);  
-        return count;
+        cnt += mergeSort(arr, low, mid);      // Sort left half
+        cnt += mergeSort(arr, mid + 1, high); // Sort right half
+        cnt += countPairs(arr, low, mid, high); // Count reverse pairs
+        merge(arr, low, mid, high);           // Merge both halves
+        return cnt;
     }
 
     private int countPairs(int[] arr, int low, int mid, int high) {
-        int count = 0, right = mid + 1;
+        int right = mid + 1;
+        int cnt = 0;
         for (int i = low; i <= mid; i++) {
-            while (right <= high && arr[i] > 2L * arr[right]) 
-                right++;  
-            count += (right - (mid + 1));
+            while (right <= high && (long)arr[i] > 2L * arr[right]) right++;
+            cnt += (right - (mid + 1));
         }
-        return count;
+        return cnt;
     }
 
     private void merge(int[] arr, int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
         int left = low, right = mid + 1;
 
         while (left <= mid && right <= high) {
@@ -37,11 +36,8 @@ class Solution {
             }
         }
 
-        while (left <= mid) 
-            temp.add(arr[left++]);
-
-        while (right <= high) 
-            temp.add(arr[right++]);
+        while (left <= mid) temp.add(arr[left++]);
+        while (right <= high) temp.add(arr[right++]);
 
         for (int i = low; i <= high; i++) {
             arr[i] = temp.get(i - low);
