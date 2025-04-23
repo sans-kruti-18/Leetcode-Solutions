f76ccf16c111fbf5ac16020647b0a9aa4ed9ebcf
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2025-04-23 18:16:21 +0530

    Time: 1 ms (89.52%), Space: 45.3 MB (22.22%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,35 +15,25 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-         // Create a list of lists to store levels
+         
         List<List<Integer>> ans = new ArrayList<>();
         if (root == null) {
-            // If the tree is empty,
-            // return an empty list
             return ans;
         }
 
-        // Create a queue to store nodes
-        // for level-order traversal
         Queue<TreeNode> q = new LinkedList<>();
-        // Push the root node to the queue
+        
         q.add(root);
 
         while (!q.isEmpty()) {
-            // Get the size of the current level
+            
             int size = q.size();
-            // Create a list to store
-            // nodes at the current level
             List<Integer> level = new ArrayList<>();
 
             for (int i = 0; i < size; i++) {
-                // Get the front node in the queue
                 TreeNode node = q.poll();
-                // Store the node value
-                // in the current level list
                 level.add(node.val);
 
-                // Enqueue the child nodes if they exist
                 if (node.left != null) {
                     q.add(node.left);
                 }
@@ -51,12 +41,10 @@ class Solution {
                     q.add(node.right);
                 }
             }
-            // Store the current level
-            // in the answer list
+            
             ans.add(level);
         }
-        // Return the level-order
-        // traversal of the tree
+        
         return ans;
         
     }
