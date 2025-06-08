92bba017e757a45798c27e996a885d4bd2097caa
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2025-06-08 20:52:13 +0530

    Time: 0 ms (100%), Space: 50.9 MB (8.71%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        int n = arr.length; // Size of the array.
+
+        // Edge cases:
+        if (n == 1)
+            return arr[0];
+        if (arr[0]!=arr[1])
+            return arr[0];
+        if (arr[n-1]!=arr[n-2])
+            return arr[n - 1];
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // If arr[mid] is the single element:
+            if (arr[mid]!=arr[mid + 1] && arr[mid]!=arr[mid - 1]) {
+                return arr[mid];
+            }
+
+            // We are in the left:
+            if ((mid % 2 == 1 && arr[mid]==arr[mid - 1])
+                    || (mid % 2 == 0 && arr[mid]==arr[mid + 1])) {
+                // Eliminate the left half:
+                low = mid + 1;
+            }
+            // We are in the right:
+            else {
+                // Eliminate the right half:
+                high = mid - 1;
+            }
+        }
+
+        // Dummy return statement:
+        return -1;
+    }
+}
