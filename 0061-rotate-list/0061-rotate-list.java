c0732c22b03b95f18bae372e4a847af56f147e23
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2025-12-04 19:25:52 +0530

    Time: 0 ms (100%), Space: 43.8 MB (47%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -14,10 +14,11 @@ class Solution {
          if(head == null||head.next == null||k == 0) return head;
          ListNode temp = head;
          int length = 1;
-         while(temp.next != null) {
-        ++length;
-        temp = temp.next;
-    }
+         while(temp.next != null)
+         {
+            length++;
+            temp = temp.next;
+         }
    
     temp.next = head;
     k = k%length; 
