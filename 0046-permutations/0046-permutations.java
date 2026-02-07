27f6390df9adeb187cc9d5a3e49c7049d7f17f66
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-02-08 00:19:20 +0530

    Time: 1 ms (86.96%), Space: 45.6 MB (44.46%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,38 +1,37 @@
 class Solution {
-
-    public List<List<Integer>> permute(int[] nums)
-     {
-        List < List < Integer >> ans = new ArrayList < > ();
-        recurPermute(0, nums, ans);
-        return ans;
-        
-     }
-
-    private void recurPermute(int index, int[] nums, List < List < Integer >> ans)
-     {
-        if (index == nums.length)
-         {
-            // copy the ds to ans
-            List < Integer > ds = new ArrayList < > ();
-            for (int i = 0; i < nums.length; i++)
-             {
-                ds.add(nums[i]);
-            }
-            ans.add(new ArrayList < > (ds));
+    
+    public void recurPerm(int ind,int[] nums,List<List<Integer>> ans)
+    {
+        if(ind==nums.length)
+        {
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++)
+             ds.add(nums[i]);
+            
+            ans.add(new ArrayList<>(ds));
             return;
+            
         }
-        for (int i = index; i < nums.length; i++)
-         {
-            swap(i, index, nums);
-            recurPermute(index + 1, nums, ans);
-            swap(i, index, nums);
+
+        for(int i=ind;i<nums.length;i++)
+        {
+            swap(i,ind,nums);
+            recurPerm(ind+1,nums,ans);
+            swap(i,ind,nums);
         }
     }
-    
-    private void swap(int i, int j, int[] nums) 
+
+    private void swap(int i,int j,int[] nums) 
     {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]= temp;
+    }
+      
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        recurPerm(0,nums,ans);
+        return ans;
+        
     }
 }
