d75e029fd642f5694c50a9e1253389c916bae318
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-03-16 11:16:39 +0530

    Time: 0 ms (100%), Space: 44.4 MB (91.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,45 +1,50 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        
-        int n = nums.length;
-        int ind = -1; // breakpoint
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                ind = i;
-                break;
-            }
-        }
-
-        // no breakpoint is found, reverse the whole array
-        if (ind == -1) {
-            reverse(nums, 0, n - 1);
-            return;
-        }
-
-        // next greater element to swap with nums[ind]
-        for (int i = n - 1; i > ind; i--) {
-            if (nums[i] > nums[ind]) {
-                swap(nums, i, ind);
-                break;
-            }
-        }
-
-        // reverse the portion after the breakpoint
-        reverse(nums, ind + 1, n - 1);
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
+    
+    public void reverse(int start,int end,int[] nums)
+    {
+        while(start<end)
+        {
+            swap(start,end,nums);
             start++;
             end--;
         }
+    }
+
+    public void swap(int i,int j,int[] nums)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    
+    public void nextPermutation(int[] nums) {
+        int ind=-1;
+
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+             {
+                ind=i;
+                break;
+             }
+        }
+
+        if(ind==-1)
+        {
+            reverse(0,nums.length-1,nums);
+            return;
+        }
+        
+        for(int i=nums.length-1;i>ind;i--)
+        {
+            if(nums[i]>nums[ind])
+            {
+                swap(i,ind,nums);
+                break;
+            }
+        }
+
+        reverse(ind+1,nums.length-1,nums);
+        
     }
 }
