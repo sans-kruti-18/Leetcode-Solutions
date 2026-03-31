e4e29eae5cd69996a06b94e875c630b3329ae66c
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-03-31 15:47:44 +0530

    Time: 2 ms (99.87%), Space: 52.3 MB (79.17%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,51 +1,53 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        int cnt1 = 0, cnt2 = 0; 
-        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
 
-        // Extended Boyer-Moore Voting Algorithm
-        for (int num : nums)
-         {
-            if (cnt1 == 0 && el2 != num) 
-            {
-                cnt1 = 1;
-                el1 = num;
-            }
-             else if (cnt2 == 0 && el1 != num) 
+        int n=nums.length;
+        int cnt1=0,cnt2=0;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+        
+        for(int num:nums)
+        {
+            if(cnt1==0 && ele2!=num)
              {
-                cnt2 = 1;
-                el2 = num;
-            } 
-            else if (num == el1) 
-            {
-                cnt1++;
-            } 
-            else if (num == el2)
+                ele1=num;
+                cnt1=1;
+             }
+            else if(cnt2==0 && ele1!=num)
              {
-                cnt2++;
-            } 
-            else 
+                ele2=num;
+                cnt2=1;
+             }
+            else if(num==ele1)
+              cnt1++;
+            else if(num==ele2)
+             cnt2++;
+            else
             {
                 cnt1--;
                 cnt2--;
             }
         }
 
-        // Validate the candidates
-        cnt1 = 0;
-        cnt2 = 0;
-        for (int num : nums) 
+        cnt1=0;
+        cnt2=0;
+        for(int num:nums)
         {
-            if (num == el1) cnt1++;
-            if (num == el2) cnt2++;
+            if(num==ele1)
+             cnt1++;
+            if(num==ele2)
+             cnt2++;
         }
 
-        List<Integer> result = new ArrayList<>();
-        int threshold = n / 3;
-        if (cnt1 > threshold) result.add(el1);
-        if (cnt2 > threshold) result.add(el2);
+        List<Integer> res = new ArrayList<>();
+        int th=n/3;
 
-        return result;
+        if(cnt1 > th)
+         res.add(ele1);
+        if(cnt2 > th)
+         res.add(ele2);
+
+        return res;
+
     }
 }
