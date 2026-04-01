3609fd1c5b239f65ca088202c3a28a751a73a69b
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-04-01 15:51:34 +0530

    Time: 21 ms (42.27%), Space: 45.7 MB (83.13%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,48 +1,42 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        
-        int n = nums.length; // size of the array
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // sort the given array:
         Arrays.sort(nums);
+        int n= nums.length;
 
-        // calculating the quadruplets:
-        for (int i = 0; i < n; i++) {
-            // avoid the duplicates while moving i:
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n; j++) {
-                // avoid the duplicates while moving j:
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+        List<List<Integer>> ans= new ArrayList<>();
 
-                // 2 pointers:
-                int k = j + 1;
-                int l = n - 1;
-                while (k < l) {
-                    long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if (sum == target) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add(nums[l]);
-                        ans.add(temp);
-                        k++;
-                        l--;
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+             continue;
 
-                        // skip the duplicates:
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
+            for(int j=i+1;j<n;j++)
+            {
+                if(j>i+1 && nums[j]==nums[j-1])
+                 continue;
+
+                int left=j+1,right=n-1;
+
+                while(left<right)
+                {
+                    long sum =(long) nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target)
+                    {
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        left++;
+                        right--;
+                        while(left<right && nums[left]==nums[left-1])
+                         left++;
+                        while(left<right && nums[right]==nums[right+1])
+                         right--;
                     }
-                    else if (sum < target) k++;
-                    else l--;
+                    else if(sum<target)
+                     left++;
+                    else
+                     right--;
                 }
             }
         }
-
         return ans;
         
     }
