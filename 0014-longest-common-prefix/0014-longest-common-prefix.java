86c7cf1169ff6a5fa85eca81db5f4fa6d3a6fa61
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-04-21 18:10:34 +0530

    Time: 1 ms (63.1%), Space: 42.8 MB (96.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,21 +1,33 @@
+import java.util.Arrays;
+
 class Solution {
-    public String longestCommonPrefix(String[] s) {
+    // Returns the longest common prefix in an array of strings
+    public String longestCommonPrefix(String[] v) {
+        // To store the result prefix
+        StringBuilder ans = new StringBuilder();
 
-        StringBuilder ans= new StringBuilder();
-        Arrays.sort(s);
+        // Sort the array of strings
+        Arrays.sort(v);
 
-        String first = s[0];
-        String last = s[s.length - 1];
+        // First string after sorting
+        String first = v[0];
 
-        for(int i=0; i < Math.min(first.length(),last.length());i++)
-        {
-            if(first.charAt(i)!=last.charAt(i))
-             return ans.toString();
+        // Last string after sorting
+        String last = v[v.length - 1];
 
+        // Compare characters of the first and last strings
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            // Stop if characters are different
+            if (first.charAt(i) != last.charAt(i)) {
+                return ans.toString();
+            }
+
+            // Add matching character to result
             ans.append(first.charAt(i));
         }
 
+        // Return the final common prefix
         return ans.toString();
-        
     }
 }
+
