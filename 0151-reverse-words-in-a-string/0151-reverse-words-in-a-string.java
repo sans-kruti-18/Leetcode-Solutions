3de042e3e2d51896a404803137d3f8ec325e5d25
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-04-24 18:39:32 +0530

    Time: 8 ms (38.51%), Space: 44.3 MB (71.67%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,36 +1,54 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder res = new StringBuilder();
+        // StringBuilder res = new StringBuilder();
 
-        int i=s.length()-1;
+        // int i=s.length()-1;
 
-        while(i>=0)
+        // while(i>=0)
+        // {
+        //     while(i>=0 && s.charAt(i)==' ')
+        //     { 
+        //         i--;
+        //     }
+
+        //     if(i<0)
+        //      break;
+
+        //     int end=i;
+
+        //     while(i>=0 && s.charAt(i)!=' ')
+        //     {
+        //         i--;
+        //     }
+
+        //     String word = s.substring(i+1,end+1);
+
+        //     if(res.length()>0)
+        //     {
+        //         res.append(" ");
+        //     }
+
+        //     res.append(word);
+        // }
+
+        // return res.toString();
+
+        String[] words= s.trim().split("\\s+");
+        int n=words.length;
+
+        int start=0,end=n-1;
+
+        while(start<end)
         {
-            while(i>=0 && s.charAt(i)==' ')
-            { 
-                i--;
-            }
-
-            if(i<0)
-             break;
-
-            int end=i;
-
-            while(i>=0 && s.charAt(i)!=' ')
-            {
-                i--;
-            }
-
-            String word = s.substring(i+1,end+1);
-
-            if(res.length()>0)
-            {
-                res.append(" ");
-            }
-
-            res.append(word);
+            String temp=words[start];
+            words[start]=words[end];
+            words[end]=temp;
+            start++;
+            end--;
         }
 
-        return res.toString();
+        return String.join(" ", words);   
+
+
     }
 }
