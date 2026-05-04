b617751eec13671a1a272b193a28d33e32d33132
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-05-04 16:09:20 +0530

    Time: 98 ms (52.01%), Space: 170.2 MB (17.61%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -15,33 +15,26 @@
  */
 class Solution {
     public int amountOfTime(TreeNode root, int target) {
-        // Map to build an undirected graph (adjacency list)
+       
         Map<Integer, List<Integer>> graph = new HashMap<>();
-
-        // Build graph from binary tree
+        
         buildGraph(root, null, graph);
-
-        // Set to track visited (burned) nodes
+        
         Set<Integer> visited = new HashSet<>();
 
-        // Queue for BFS starting from the target node
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(target);
         visited.add(target);
 
-        // Variable to count total time taken to burn all nodes
         int time = 0;
 
-        // BFS level order traversal to simulate fire spread
         while (!queue.isEmpty()) {
             int size = queue.size();
             boolean burned = false;
 
-            // Process all nodes at the current level
             for (int i = 0; i < size; i++) {
                 int node = queue.poll();
-
-                // Traverse all connected neighbors of the current node
+             
                 for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
                     if (!visited.contains(neighbor)) {
                         visited.add(neighbor);
@@ -51,19 +44,17 @@ class Solution {
                 }
             }
 
-            // If new nodes got burned, increment time
             if (burned) {
                 time++;
             }
         }
 
-        // Return total time taken to burn the binary tree
         return time;
     }
 
-    // Helper method to build an undirected graph from binary tree
+   
     private void buildGraph(TreeNode node, TreeNode parent, Map<Integer, List<Integer>> graph) {
-        // Base case: if current node is null, return
+       
         if (node == null) return;
 
         // If parent exists, add bidirectional connection
