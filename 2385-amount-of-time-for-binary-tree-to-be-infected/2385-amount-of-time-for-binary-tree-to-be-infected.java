24548d3fb3cd7e81f728baabfb20ca7f46a87303
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-05-04 16:12:50 +0530

    Time: 116 ms (25.63%), Space: 169.9 MB (17.67%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -1,24 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int amountOfTime(TreeNode root, int target) {
        
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, List<Integer>> mapToParent = new HashMap<>();
         
-        buildGraph(root, null, graph);
+        buildGraph(root, null, mapToParent);
         
         Set<Integer> visited = new HashSet<>();
 
@@ -35,7 +20,7 @@ class Solution {
             for (int i = 0; i < size; i++) {
                 int node = queue.poll();
              
-                for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
+                for (int neighbor : mapToParent.getOrDefault(node, new ArrayList<>())) {
                     if (!visited.contains(neighbor)) {
                         visited.add(neighbor);
                         queue.offer(neighbor);
@@ -53,18 +38,18 @@ class Solution {
     }
 
    
-    private void buildGraph(TreeNode node, TreeNode parent, Map<Integer, List<Integer>> graph) {
+    private void buildGraph(TreeNode node, TreeNode parent, Map<Integer, List<Integer>> mapToParent) {
        
         if (node == null) return;
 
         // If parent exists, add bidirectional connection
         if (parent != null) {
-            graph.computeIfAbsent(node.val, k -> new ArrayList<>()).add(parent.val);
-            graph.computeIfAbsent(parent.val, k -> new ArrayList<>()).add(node.val);
+            mapToParent.computeIfAbsent(node.val, k -> new ArrayList<>()).add(parent.val);
+            mapToParent.computeIfAbsent(parent.val, k -> new ArrayList<>()).add(node.val);
         }
 
-        // Recursively build graph for left and right children
-        buildGraph(node.left, node, graph);
-        buildGraph(node.right, node, graph);
+        // Recursively build mapToParent for left and right children
+        buildGraph(node.left, node, mapToParent);
+        buildGraph(node.right, node, mapToParent);
     }
 }
