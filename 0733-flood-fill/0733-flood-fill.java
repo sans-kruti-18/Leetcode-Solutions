dfd5eb7602a59c7faa480cdb38b37bf2ed1aa9dc
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-05-08 12:33:16 +0530

    Time: 0 ms (100%), Space: 46.6 MB (89.56%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    public void dfs(int row,int col,int[][] image,int[][] ans,int[] drow,int[] dcol,int iniCol,int newCol)
+    {
+        int n=image.length;
+        int m=image[0].length;
+
+        ans[row][col]= newCol;
+
+        for(int i=0;i<4;i++)
+        {
+            int nrow= row+drow[i];
+            int ncol= col+dcol[i];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniCol && ans[nrow][ncol]!=newCol)
+              dfs(nrow,ncol,image,ans,drow,dcol,iniCol,newCol);
+
+        }
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        int iniCol= image[sr][sc];
+        int[][] ans= image;
+        int[] drow={-1,0,1,0};
+        int[] dcol={0,1,0,-1};
+
+        dfs(sr,sc,image,ans,drow,dcol,iniCol,color);
+
+        return ans;
+    }
+}
