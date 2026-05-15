1981d40eebaf7249905f83027b9cee5f119a7ef5
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-05-15 16:01:20 +0530

    Time: 5 ms (76.05%), Space: 65.9 MB (77.72%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,70 +1,68 @@
-import java.util.*;
+class Solution {
 
-public class Solution {
+    int find(int[] parent,int x)
+    {
+        if(parent[x]!=x)
+          parent[x]=find(parent,parent[x]);
 
-    public int makeConnected(int n, int[][] connections) {
-
-        int[] parent = new int[n];
-        int[] rank = new int[n];
-
-        // Initialize parent
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-
-        int extraEdges = 0;
-
-        // Process each connection
-        for (int[] edge : connections) {
-            int u = edge[0];
-            int v = edge[1];
-
-            int pu = find(parent, u);
-            int pv = find(parent, v);
-
-            // If both have same parent → extra edge
-            if (pu == pv) {
-                extraEdges++;
-            } else {
-                union(parent, rank, pu, pv);
-            }
-        }
-
-        // Count number of components
-        int components = 0;
-        for (int i = 0; i < n; i++) {
-            if (find(parent, i) == i) {
-                components++;
-            }
-        }
-
-        int neededEdges = components - 1;
-
-        // Check if we can connect all components
-        if (extraEdges >= neededEdges) {
-            return neededEdges;
-        }
-
-        return -1;
-    }
-
-    // Find with path compression
-    private int find(int[] parent, int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent, parent[x]);
-        }
         return parent[x];
     }
 
-    // Union by rank
-    private void union(int[] parent, int[] rank, int x, int y) {
-        if (rank[x] < rank[y]) {
-            parent[x] = y;
-        } else if (rank[x] > rank[y]) {
-            parent[y] = x;
-        } else {
-            parent[y] = x;
+    void union(int x,int y, int[] parent,int[] rank)
+    {
+        if(rank[x] < rank[y])
+          parent[x]=y;
+
+        else if(rank[x] > rank[y])
+          parent[y]=x;
+
+        else
+        {
+            parent[y]=x;
             rank[x]++;
         }
+    }
+
+    public int makeConnected(int n, int[][] conn) {
+        if(conn.length < n-1)
+         return -1;
+
+        int[] parent= new int[n];
+        int[] rank= new int[n];
+
+        for(int i=0;i<n;i++)
+          parent[i]=i;
+
+        int extra=0;
+
+        for(int[] edge: conn)
+        {
+            int u=edge[0];
+            int v=edge[1];
+
+            int pu= find(parent,u);
+            int pv= find(parent,v);
+
+            if(pu==pv)
+             extra++;
+
+            else
+             union(pu,pv,parent,rank);
+        }
+
+        int compo=0;
+        for(int i=0;i<n;i++)
+        {
+            if(parent[i]==i)
+              compo++;
+        }
+
+        if(extra >= compo-1)
+         return compo-1;
+
+        return -1;
+        
+
+        
     }
 }
