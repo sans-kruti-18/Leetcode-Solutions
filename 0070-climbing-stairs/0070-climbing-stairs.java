e1847c7f8510c4bb9c3d26f1bf35de6ddb7bf743
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-05-17 17:24:07 +0530

    Time: 0 ms (100%), Space: 42 MB (53.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int climbStairs(int n) {
-
-        if (n <= 2) return n;
-
-        int prev2 = 1;
-        int prev1 = 2;
-
-        for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        
+        if (n <= 1) {
+            return 1;
         }
 
-        return prev1;
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 }
