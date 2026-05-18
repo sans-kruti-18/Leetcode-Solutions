b8b435d37ecc7c246fb832e72e8a2c5e8c79aaf6
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-05-18 20:45:29 +0530

    Time: 0 ms (100%), Space: 43.6 MB (44.75%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n=obstacleGrid.length;
+        int m=obstacleGrid[0].length;
+
+        int[][] dp = new int[n][m];
+
+        return solve(n,m,obstacleGrid,dp);
+    }
+
+    public int solve(int n,int m,int[][] obstacleGrid,int[][] dp)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(obstacleGrid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+
+                int up=0;
+                int left=0;
+
+                if(i>0)
+                 up= dp[i-1][j];
+                if(j>0)
+                 left= dp[i][j-1];
+
+                dp[i][j]= up+left;
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+}
