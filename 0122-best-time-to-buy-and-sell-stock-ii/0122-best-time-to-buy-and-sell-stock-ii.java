a743ad525da85b26aeb62152e5802a34fd860193
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-05-26 12:13:17 +0530

    Time: 2 ms (15.51%), Space: 46.3 MB (81.56%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,36 +1,66 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n= prices.length;
 
-        int[][] dp= new int[n][2];
-
-        for(int[] row:dp)
-         Arrays.fill(row,-1);
-
-        return solve(prices,0,0,n,dp);        
-    }
-
-    public int solve(int[] prices,int ind,int buy,int n,int[][] dp)
+    public int maxProfit(int[] prices)
     {
-        if(ind==n)
+        int n=prices.length;
+        if(n==0)
          return 0;
 
-        if(dp[ind][buy] != -1)
-         return dp[ind][buy];
+        int[][] dp = new int[n+1][2];
 
-        int profit;
+        dp[n][0]=dp[n][1]=0;
 
-        if(buy==0)
+        for(int ind=n-1;ind>=0;ind--)
         {
-            profit = Math.max(0+solve(prices,ind+1,0,n,dp), -prices[ind]+ solve(prices,ind+1,1,n,dp));
-        }
-        else
-        {
-            profit = Math.max(solve(prices,ind+1,1,n,dp), prices[ind]+ solve(prices,ind+1,0,n,dp));
+            for(int buy=0;buy<=1;buy++)
+            {
+                int profit;
+
+                if(buy==0)
+                 profit= Math.max(0 + dp[ind+1][0], -prices[ind]+ dp[ind+1][1]);
+                else
+                 profit=Math.max(0 + dp[ind+1][1], prices[ind]+ dp[ind+1][0]);
+
+                dp[ind][buy]=profit;
+            }
         }
 
-        dp[ind][buy]=profit;
-
-        return dp[ind][buy];
+        return dp[0][0];
     }
+
+
+    // public int maxProfit(int[] prices) {
+    //     int n= prices.length;
+
+    //     int[][] dp= new int[n][2];
+
+    //     for(int[] row:dp)
+    //      Arrays.fill(row,-1);
+
+    //     return solve(prices,0,0,n,dp);        
+    // }
+
+    // public int solve(int[] prices,int ind,int buy,int n,int[][] dp)
+    // {
+    //     if(ind==n)
+    //      return 0;
+
+    //     if(dp[ind][buy] != -1)
+    //      return dp[ind][buy];
+
+    //     int profit;
+
+    //     if(buy==0)
+    //     {
+    //         profit = Math.max(0+solve(prices,ind+1,0,n,dp), -prices[ind]+ solve(prices,ind+1,1,n,dp));
+    //     }
+    //     else
+    //     {
+    //         profit = Math.max(solve(prices,ind+1,1,n,dp), prices[ind]+ solve(prices,ind+1,0,n,dp));
+    //     }
+
+    //     dp[ind][buy]=profit;
+
+    //     return dp[ind][buy];
+    // }
 }
