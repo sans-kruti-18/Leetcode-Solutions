b5a45b56bf983e9c0d7938b4483d2f93dbc11f98
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-06-18 15:23:37 +0530

    Time: 4 ms (38.92%), Space: 43.2 MB (72.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-        for (char it : s.toCharArray()) {
-            if (it == '(' || it == '[' || it == '{')
-                st.push(it);
-            else {
-                if(st.isEmpty()) return false;
-                char ch = st.pop(); 
-                if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
-                else return false;
-            }
+        Stack<Character> st= new Stack<>();
+
+        for(char ch:s.toCharArray())
+        {
+            if(ch=='(' || ch=='{' || ch=='[')
+             st.push(ch);
+
+            else
+             {
+                if(st.isEmpty())
+                 return false;
+
+                char top=st.pop();
+
+                if((ch==')' && top=='(') || (ch=='}' && top=='{') || (ch==']' && top=='['))
+                 continue;
+
+                else
+                 return false;
+             }
         }
+
         return st.isEmpty();
+        
     }
 }
