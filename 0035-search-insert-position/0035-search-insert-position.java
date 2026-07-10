fffea08d1f060723529174c0324384f1f8f78545
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-07-10 15:42:36 +0530

    Time: 0 ms (100%), Space: 44.8 MB (28.66%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,13 +1,19 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int i;
-        for (i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
-            } else if (nums[i] > target) {
-                return i;
+    public int searchInsert(int[] arr, int x) {
+        int low = 0;                  // Start index
+        int high = arr.length - 1;    // End index
+        int ans = arr.length;         // Default value if not found
+
+        while (low <= high) {
+            int mid = (low + high) / 2;  // Find mid index
+
+            if (arr[mid] >= x) {
+                ans = mid;            // Store possible answer
+                high = mid - 1;       // Move left
+            } else {
+                low = mid + 1;        // Move right
             }
         }
-        return i;
+        return ans; 
     }
 }
