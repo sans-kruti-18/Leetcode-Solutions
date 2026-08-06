7de0f9a4c0e82871990a9ed615a514007f8b49e5
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-08-06 14:54:35 +0530

    Time: 1 ms (100%), Space: 49 MB (16.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,60 @@
-// class Solution {
-//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-//     }
-// }
-
 class Solution {
-    public double findMedianSortedArrays(int[] a, int[] b) {
-        int n1 = a.length;
-        int n2 = b.length;
-        //if n1 is bigger swap the arrays:
-        if (n1 > n2) return findMedianSortedArrays(b, a);
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int i=0,j=0;
+        int n=n1+n2;
+        int ind2=n/2;
+        int ind1=ind2-1;
+        int ele1=-1,ele2=-1;
+        int cnt=0;
 
-        int n = n1 + n2; //total length
-        int left = (n1 + n2 + 1) / 2; //length of left half
-        //apply binary search:
-        int low = 0, high = n1;
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
-            //calculate l1, l2, r1 and r2;
-            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
-            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
-            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+        while(i<n1 && j<n2)
+        {
+            if(nums1[i]<nums2[j])
+            {
+                if(cnt==ind1)
+                 ele1=nums1[i];
+                if(cnt==ind2)
+                 ele2=nums1[i];
+                cnt++;
+                i++;
+            }
 
-            if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 1) return Math.max(l1, l2);
-                else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
-            } else if (l1 > r2) high = mid1 - 1;
-            else low = mid1 + 1;
+            else
+            {
+                if(cnt==ind1)
+                 ele1=nums2[j];
+                if(cnt==ind2)
+                 ele2=nums2[j];
+                cnt++;
+                j++; 
+            }
         }
-        return 0; //dummy statement
+
+        while(i<n1)
+        {
+             if(cnt==ind1)
+                 ele1=nums1[i];
+                if(cnt==ind2)
+                 ele2=nums1[i];
+                cnt++;
+                i++;
+        }
+
+        while(j<n2)
+        {
+            if(cnt==ind1)
+                 ele1=nums2[j];
+                if(cnt==ind2)
+                 ele2=nums2[j];
+                cnt++;
+                j++; 
+        }
+
+        if(n%2==1)
+         return ele2;
+
+        return (double)((double)(ele1+ele2)/2.0);
     }
 }
