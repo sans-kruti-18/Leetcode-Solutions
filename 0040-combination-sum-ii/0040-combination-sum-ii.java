1add25258d7fc6c3ecb321d929cccb3e8f664446
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-08-20 13:51:37 +0530

    Time: 9 ms (15.64%), Space: 45.3 MB (40.39%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,23 +1,33 @@
 class Solution {
-    static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
-        if (target == 0) {
-            ans.add(new ArrayList < > (ds));
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        Arrays.sort(arr);
+
+        findComb(0,target,res,ds,arr);
+        return res;
+    }
+
+    public void findComb(int ind,int target,List<List<Integer>> res,List<Integer> ds,int[] arr)
+    {
+        if(target==0)
+        {
+            res.add(new ArrayList<>(ds));
             return;
         }
 
-        for (int i = ind; i < arr.length; i++) {
-            if (i > ind && arr[i] == arr[i - 1]) continue;
-            if (arr[i] > target) break;
+        if(ind==arr.length || target<0)
+         return;
 
-            ds.add(arr[i]);
-            findCombinations(i + 1, arr, target - arr[i], ans, ds);
-            ds.remove(ds.size() - 1);
-        }
+        ds.add(arr[ind]);
+        findComb(ind+1,target-arr[ind],res,ds,arr);
+        ds.remove(ds.size()-1);
+
+        int next=ind+1;
+        while(next<arr.length && arr[next]==arr[ind])
+         next++;
+
+        findComb(next,target,res,ds,arr);
     }
-    public static List < List < Integer >> combinationSum2(int[] candidates, int target) {
-        List < List < Integer >> ans = new ArrayList < > ();
-        Arrays.sort(candidates);
-        findCombinations(0, candidates, target, ans, new ArrayList < > ());
-        return ans;
-    }
+
 }
