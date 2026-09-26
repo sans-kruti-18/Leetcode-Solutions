47b728bae916e1ee0770e975a6e6963efce661e2
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-09-26 19:08:06 +0530

    Time: 72 ms (83%), Space: 154.8 MB (59.21%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -0,0 +1,65 @@
+class Solution {
+
+    public int mostBooked(int n, int[][] meetings) {
+
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<Integer> freeRooms = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            freeRooms.offer(i);
+        }
+
+        PriorityQueue<long[]> busyRooms =
+                new PriorityQueue<>(
+                        (a, b) -> a[0] == b[0]
+                                ? Long.compare(a[1], b[1])
+                                : Long.compare(a[0], b[0]));
+
+        long[] count = new long[n];
+
+        for (int[] meeting : meetings) {
+
+            int start = meeting[0];
+            int end = meeting[1];
+            int duration = end - start;
+
+            while (!busyRooms.isEmpty()
+                    && busyRooms.peek()[0] <= start) {
+
+                freeRooms.offer((int) busyRooms.poll()[1]);
+            }
+
+            if (!freeRooms.isEmpty()) {
+
+                int room = freeRooms.poll();
+
+                busyRooms.offer(new long[]{end, room});
+
+                count[room]++;
+            }
+            else {
+
+                long[] earliest = busyRooms.poll();
+
+                long freeTime = earliest[0];
+                int room = (int) earliest[1];
+
+                busyRooms.offer(
+                        new long[]{freeTime + duration, room});
+
+                count[room]++;
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (count[i] > count[ans]) {
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+}
