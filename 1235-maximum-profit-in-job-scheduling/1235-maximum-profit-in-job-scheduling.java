4a8e29a32d004fff99f40b18d1782b2d50e62395
sans-kruti-18 <166274894+sans-kruti-18@users.noreply.github.com>
2026-09-26 17:36:32 +0530

    Time: 36 ms (49.12%), Space: 76.9 MB (14.41%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,23 +1,64 @@
 class Solution {
+
+    int[][] jobs;
+    Integer[] dp;
+
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+
         int n = startTime.length;
-        int[][] jobs = new int[n][3];
-        
-        for (int i = 0; i < n; i++) 
-            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
-        
-        Arrays.sort(jobs, (a, b)->a[1] - b[1]);
-        
-        TreeMap<Integer, Integer> dp = new TreeMap<>();
-        dp.put(0, 0);
-        
-        for (int[] job : jobs) 
-        {
-            int cur = dp.floorEntry(job[0]).getValue() + job[2];
-            if (cur > dp.lastEntry().getValue())
-                dp.put(job[1], cur);
+
+        jobs = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = startTime[i];
+            jobs[i][1] = endTime[i];
+            jobs[i][2] = profit[i];
         }
-        
-        return dp.lastEntry().getValue();
+
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+
+        dp = new Integer[n];
+
+        return solve(0);
+    }
+
+    private int solve(int idx) {
+
+        if (idx >= jobs.length)
+            return 0;
+
+        if (dp[idx] != null)
+            return dp[idx];
+
+        // Take current job
+        int nextIdx = findNextJob(jobs[idx][1]);
+
+        int take = jobs[idx][2] + solve(nextIdx);
+
+        // Skip current job
+        int notTake = solve(idx + 1);
+
+        return dp[idx] = Math.max(take, notTake);
+    }
+
+    private int findNextJob(int endTime) {
+
+        int low = 0;
+        int high = jobs.length - 1;
+        int ans = jobs.length;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (jobs[mid][0] >= endTime) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 }
